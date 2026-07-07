2f6dab6d199e8198b3cdbf27abf92533c0e80f3f
Leandro Melchiori <leandromelchiori@gmail.com>
2026-07-07 10:33:05 -0300

    Inline todo icons

--- src/components/TodoIcon/TodoIcon.jsx
+++ src/components/TodoIcon/TodoIcon.jsx
@@ -1,18 +1,47 @@
-import { IoIosCheckmarkCircle as CheckSVG } from "react-icons/io";
-import { GiCancel as DeleteSVG } from "react-icons/gi";
-import { MdEdit as EditSVG } from "react-icons/md";
-import { MdKeyboardArrowDown as MoveDownSVG, MdKeyboardArrowUp as MoveUpSVG } from "react-icons/md";
 import './TodoIcon.css';
 
 const iconTypes = {
-    "check": (color) => <CheckSVG className="icon-svg" fill={color} />,
-    "delete": (color) => <DeleteSVG className="icon-svg" fill={color} />,
-    "edit": (color) => <EditSVG className="icon-svg" fill={color} />,
-    "moveDown": (color) => <MoveDownSVG className="icon-svg" fill={color} />,
-    "moveUp": (color) => <MoveUpSVG className="icon-svg" fill={color} />
+    check: {
+        paths: ['M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1.15 14.15-4-4 1.42-1.42 2.58 2.59 5.88-5.89 1.42 1.42-7.3 7.3z'],
+        viewBox: '0 0 24 24',
+    },
+    delete: {
+        paths: ['M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20zm4.24 12.83-1.41 1.41L12 13.41l-2.83 2.83-1.41-1.41L10.59 12 7.76 9.17l1.41-1.41L12 10.59l2.83-2.83 1.41 1.41L13.41 12l2.83 2.83z'],
+        viewBox: '0 0 24 24',
+    },
+    edit: {
+        paths: ['M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zm2 1.58 9.06-9.06 1.17 1.17L6.17 20H5v-1.17z', 'M18.71 8.04a1 1 0 0 0 0-1.41l-1.34-1.34a1 1 0 0 0-1.41 0l-1.05 1.05 3.75 3.75 1.05-1.05z'],
+        viewBox: '0 0 24 24',
+    },
+    moveDown: {
+        paths: ['M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z'],
+        viewBox: '0 0 24 24',
+    },
+    moveUp: {
+        paths: ['M7.41 15.41 12 10.83l4.59 4.58L18 14l-6-6-6 6 1.41 1.41z'],
+        viewBox: '0 0 24 24',
+    },
+};
+
+function IconSvg({ color, icon }) {
+    return (
+        <svg
+            aria-hidden="true"
+            className="icon-svg"
+            fill={color}
+            focusable="false"
+            viewBox={icon.viewBox}
+        >
+            {icon.paths.map(path => (
+                <path d={path} key={path} />
+            ))}
+        </svg>
+    );
 }
 
 function TodoIcon({ disabled = false, type, color, label, onClick }) {
+    const icon = iconTypes[type];
+
     return (
         <button
             type="button"
@@ -21,7 +50,7 @@ function TodoIcon({ disabled = false, type, color, label, onClick }) {
             disabled={disabled}
             onClick={onClick}
         >
-            {iconTypes[type](color)}
+            <IconSvg color={color} icon={icon} />
         </button>
     );
 }
